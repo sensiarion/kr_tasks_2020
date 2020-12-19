2794e6653c2aaa7c73377b20fb4b240283448cfc
ikinichi <ichigo7336@gmail.com>
2020-12-19 15:45:36 +0500

    fourth done

--- third/main.py
+++ third/main.py
@@ -1,32 +1,20 @@
-line = int(input())
-data = input()
-rows = [i.split(')&') for i in data.split('#!^')]
+import sys
 
-for i, row in enumerate(rows):
+raw = sys.stdin.read().split('\n')
+
+table = []
+
+for row in raw:
+    table.append([int(i) for i in row.split()])
+
+max_value = max([max(row) for row in table])
+min_value = min([min(row) for row in table])
+
+result = {max_value:[],min_value:[]}
+
+for i,row in enumerate(table):
     for j, element in enumerate(row):
-        if element.isdigit():
-            continue
-        elif element == '':
-            continue
-        else:
-            res = ''
-            for k in element:
-                if k.isdigit():
-                    res += k
+        if (element == max_value) or (element == min_value):
+            result[element].append((i,j))
 
-            row[j] = res
-
-to_remove = set()
-
-for i in range(len(rows) - 1):
-    if rows[i] == rows[i + 1]:
-        to_remove.add(i)
-
-temp = []
-for i, row in enumerate(rows):
-    if i not in to_remove:
-        temp.append(row)
-
-rows = temp
-
-print(*rows[line], sep='\t')
+print(result)
